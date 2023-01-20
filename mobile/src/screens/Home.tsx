ed05d07c7567dc2d8597c72ffafdfd8ff700063f
Eduardo Rocha <eduardomfrocha@gmail.com>
2023-01-20 14:05:10 -0300

    Create Checkboxes

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { View, Text, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
@@ -12,6 +13,9 @@ const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 
 export function Home() {
+
+
+  const { navigate } = useNavigation()
   return (
     <View className="flex-1 bg-background px-8 pt-16" >
       <Header />
